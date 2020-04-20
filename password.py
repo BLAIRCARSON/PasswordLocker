aca1831774f66c9e723315b39fd27a6e4abcc7e7
BA <engineervinceblair@gmail.com>
2020-04-20 20:12:53 +0300

    Add method that save password  using .append

--- password.py
+++ password.py
@@ -8,11 +8,7 @@ class User:
         self.name = name
         self.user_password = user_password
 
-    def save_user(self):
-        """
-        save_user method saves user objects into user_list
-        """
-        User.user_list.append(self)
+   
 
     def delete_user(self):
         """
